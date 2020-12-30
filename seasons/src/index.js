c19295fb02ef4c21c66a62208a49ef0cb80b429b
Roelfie <roelfie.kerstholt@gmail.com>
2020-12-30 22:45:21 +0100

    Improved error handling.

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,23 +4,23 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { latitude: 'Unknown', errorMessage: null };
+    this.state = { latitude: null, errorMessage: null };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ latitude: position.coords.latitude }),
-      (err) => console.error(err.message)
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
-  render() { 
-    return <div>Latitude: {this.state.latitude}</div>;
+  render() {
+    if (this.state.latitude && !this.state.errorMessage) {
+      return <div>Latitude: {this.state.latitude}</div>;
+    } 
+    if (!this.state.latitude && this.state.errorMessage) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    return <div>Loading...</div>;
   }
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
-
-
-Error: {this.state.errorMessage}
-<br/>
-
-(err) => this.setState({ errorMessage: err.message })
