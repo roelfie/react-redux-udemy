2cd825d4ab1dfdedf601dbbd59cbc07305341326
Roelfie <roelfie.kerstholt@gmail.com>
2020-12-31 23:32:33 +0100

    Store result of API call in React state.

--- image-finder/src/components/App.jsx
+++ image-finder/src/components/App.jsx
@@ -3,25 +3,23 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  async findImages(searchTerm) {
-    const response = await axios.get("https://api.unsplash.com/./search/photos", {
-      params: {
-        query: searchTerm
-      },
-      headers: {
-        Authorization: "Client-ID nx6aTsWQ-I-yPt9Lbgue69MtRgn7PTVueQNkMRwb6rs"
-      }
+  state = { images: [] };
+
+  findImages = async (searchTerm) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: searchTerm },
+      headers: { Authorization: "Client-ID nx6aTsWQ-I-yPt9Lbgue69MtRgn7PTVueQNkMRwb6rs" }
     });
 
-    console.log(response.data.results);
-    // Instead of async/await we can also use a promise like axios.get().then(...):
-    //.then((response) => console.log(response.data.results));
-  }
+    this.setState({ images: response.data.results });
+    // NB: Instead of async/await we can also use a promise: axios.get().then((resp) => {...}))
+  };
 
   render() {
     return (
       <div className='ui container' style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.findImages} />
+        <div>Found: {this.state.images.length} images</div>
       </div>
     );
   }
