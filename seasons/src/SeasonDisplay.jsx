d79b6d097f1aaf400a9de166a4213defb2760026
Roelfie <roelfie.kerstholt@gmail.com>
2020-12-31 12:04:24 +0100

    Using JavaScript inside JSX expressions.

--- seasons/src/SeasonDisplay.jsx
+++ seasons/src/SeasonDisplay.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+const isSummer = (latitude, month) => {
+    if (latitude >= 0) {
+        return (month >2 && month <9);
+    } else {
+        return (month <3 || month >8);
+    }
+}
+
 const SeasonDisplay = (props) => {
-    return (<div>
-        <div>Latitude: {props.latitude}</div>
-    </div>);
+    const summer = isSummer(props.latitude, new Date().getMonth());
+    return (
+        <div>
+            <div>Season: {summer === true ? 'Summer' : 'Winter'}</div>
+        </div>
+    );
 };
 
 export default SeasonDisplay;
