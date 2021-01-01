9e19c694d5295324d128cb6e70c4d392c42b669d
Roelfie <roelfie.kerstholt@gmail.com>
2021-01-01 12:52:20 +0100

    Added grid CSS.

--- image-finder/src/components/ImageList.jsx
+++ image-finder/src/components/ImageList.jsx
@@ -1,3 +1,4 @@
+import "./ImageList.css";
 import React from "react";
 
 const ImageList = (props) => {
@@ -9,7 +10,7 @@ const ImageList = (props) => {
     );
   });
 
-  return <div>{images}</div>;
+  return <div className='image-list'>{images}</div>;
 };
 
 export default ImageList;
