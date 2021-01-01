1ba83ff86a9c7c70ae871fbb66f101ff8067fa12
Roelfie <roelfie.kerstholt@gmail.com>
2021-01-01 20:47:59 +0100

    Implement SearchBar.

--- movie-player/src/components/App.jsx
+++ movie-player/src/components/App.jsx
@@ -1,16 +1,19 @@
+import React from "react";
 import SearchBar from "./SearchBar";
 import VideoDetails from "./VideoDetails";
 import VideoList from "./VideoList";
 
-function App() {
-  return (
-    <div>
-      <h2>Movie Player</h2>
-      <SearchBar />
-      <VideoDetails />
-      <VideoList />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className='ui container'>
+        <h2>Movie Player</h2>
+        <SearchBar />
+        <VideoDetails />
+        <VideoList />
+      </div>
+    );
+  }
 }
 
 export default App;
--- movie-player/src/components/SearchBar.jsx
+++ movie-player/src/components/SearchBar.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
-const SearchBar = () => {
-  return <div>SearchBar</div>;
-};
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    // TODO Call callback from parent.
+  };
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className='search-bar ui segment'>
+        <form className='ui form' onSubmit={this.onFormSubmit}>
+          <div className='field'>
+            <label>Search Movies</label>
+            <input
+              type='text'
+              name='search-term'
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
